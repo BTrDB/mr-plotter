c30fb9ceb4e68d1ccc13ce4c509d7f6051e90aa7
Sam Kumar <samkumar@berkeley.edu>
2016-02-27 18:17:32 -0800

    Add GPL notices

--- assets/mr-plotter/js/utils.js
+++ assets/mr-plotter/js/utils.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2016 Sam Kumar, Michael Andersen, and the University
+ * of California, Berkeley.
+ *
+ * This file is part of Mr. Plotter (the Multi-Resolution Plotter).
+ *
+ * Mr. Plotter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mr. Plotter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mr. Plotter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var slashRE = new RegExp("/", "g");
 
 function formatPath(metadata) {
