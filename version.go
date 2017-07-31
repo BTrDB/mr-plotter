eefca90e3e13f9e66b02c26f11d534e9caf974e1
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-07-31 15:40:05 -0700

    Version bump

--- version.go
+++ version.go
@@ -8,5 +8,5 @@ MINOR version when you add functionality in a backwards-compatible manner, and
 PATCH version when you make backwards-compatible bug fixes.
 */
 const VersionMajor = 4
-const VersionMinor = 5
+const VersionMinor = 6
 const VersionPatch = 0
