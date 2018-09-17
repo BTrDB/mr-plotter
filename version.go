4c8dc0db4467500ac2f7a67328e2b57dcfe4c641
Michael P Andersen <michael@steelcode.com>
2018-09-17 16:17:05 -0700

    Add 4.15.0

--- version.go
+++ version.go
@@ -8,5 +8,5 @@ MINOR version when you add functionality in a backwards-compatible manner, and
 PATCH version when you make backwards-compatible bug fixes.
 */
 const VersionMajor = 4
-const VersionMinor = 14
-const VersionPatch = 3
+const VersionMinor = 15
+const VersionPatch = 0
